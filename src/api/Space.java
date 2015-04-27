b8d38ae6b0a7faa00997d032115c5234460e0ac3
magnumog <magnumog@stud.ntnu.no>
2015-04-26 18:19:32 -0700

    throws remoteException

--- src/api/Space.java
+++ src/api/Space.java
@@ -1,20 +1,22 @@
 package api;
 
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 import java.util.List;
 
 /**
  * 
  * Space interface
  */
-public interface Space {
+public interface Space extends Remote {
 	public static int PORT = 8001;
 	public static String SERVICE_NAME = "Space";
 	
-	public void putTask(Task<?> task);
-	public void putTasks(List<Task<?>> tasks);
+	public void putTask(Task<?> task) throws RemoteException;
+	public void putTasks(List<Task<?>> tasks) throws RemoteException;
 	
-	public void putResult(Result<?> result);
-	public void putResults(List<Result<?>> results);
+	public void putResult(Result<?> result) throws RemoteException;
+	public void putResults(List<Result<?>> results) throws RemoteException;
 	
 	public List<Result<?>> takeResults();
 
