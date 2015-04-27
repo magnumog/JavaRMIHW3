338da6489d072104d789edb512b4c0f8e3cfc9f8
magnumog <magnumog@stud.ntnu.no>
2015-04-26 17:25:55 -0700

    fixed packages and added unimpl methods in SpaceImpl

--- src/system/SpaceImpl.java
+++ src/system/SpaceImpl.java
@@ -2,13 +2,38 @@ package system;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.List;
 
+import api.Result;
 import api.Space;
+import api.Task;
 
 public class SpaceImpl extends UnicastRemoteObject implements Space {
 
 	protected SpaceImpl() throws RemoteException {
 		super();
+	}
+
+	@Override
+	public void putTask(Task<?> task) {
+	}
+
+	@Override
+	public void putTasks(List<Task<?>> tasks) {
+	}
+
+	@Override
+	public void putResult(Result<?> result) {
+	}
+
+	@Override
+	public void putResults(List<Result<?>> results) {
+		
+	}
+
+	@Override
+	public List<Result<?>> takeResults() {
+		return null;
 	}
 
 }
