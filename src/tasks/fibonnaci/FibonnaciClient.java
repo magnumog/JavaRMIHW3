ee5db86fbe9aaed283f1c9b4197b12cef05b564d
Terje Skow <terjeks@popeye.cs.ucsb.edu>
2015-04-28 12:42:24 -0700

    Fixed time in Fibonacci

--- src/tasks/fibonnaci/FibonnaciClient.java
+++ src/tasks/fibonnaci/FibonnaciClient.java
@@ -23,10 +23,11 @@ public class FibonnaciClient extends Client<FibonacciResult> {
 		} else {
 			domain = args[0];
 		}
-		
+		client.begin();
 		final FibonnaciClient client = new FibonnaciClient("Fibonacci", domain);
 		final FibonnaciTask task = new FibonnaciTask(16);
 		System.out.println(client.processTask(task));
+		client.end();
 		
 //		client.add();
 	}
